016ac98e86e39d53fa94266d84d112c4e4274771
LuzAneliCampos <152234994+Luz-Campos@users.noreply.github.com>
2024-05-22 00:23:08 -0600

    Add files via upload (#1)

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.carousel-marcas').slick({
+    // Configuraciones del carrusel
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
+
